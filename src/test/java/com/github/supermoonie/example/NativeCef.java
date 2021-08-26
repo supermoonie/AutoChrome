928670c60d58abd0b1f0c354248432894533784c
wang_chao <chao.wang@guanaitong.com>
2021-08-26 13:48:04 +0800

    fix localhost

--- src/test/java/com/github/supermoonie/example/NativeCef.java
+++ src/test/java/com/github/supermoonie/example/NativeCef.java
@@ -3,6 +3,9 @@ package com.github.supermoonie.example;
 import com.github.supermoonie.auto.AutoChrome;
 import org.junit.Test;
 
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author supermoonie
  * @since 2021/8/23
@@ -11,8 +14,14 @@ public class NativeCef {
 
     @Test
     public void nc() throws Exception {
-        AutoChrome autoChrome = new AutoChrome(9222, "Warcraft 3 maps for download | Warcraft 3: Reforged - Map database - Page 3", 5_000);
-        Object eval = autoChrome.eval("document.querySelector('.table-map-list').innerHTML");
-        System.out.println(eval);
+        AutoChrome autoChrome = new AutoChrome(9222, "about:blank", 5_000);
+        for (int i = 0; i < 1; i++) {
+            autoChrome.navigate("https://www.hermes.com/hk/en/product/mosaique-au-24-mini-bag-H079054CKP9/");
+            autoChrome.waitCondition(autoChrome1 -> (boolean) autoChrome1.eval("!!document.getElementById('add-to-cart-button-in-stock')"), 5_000);
+            TimeUnit.SECONDS.sleep(6);
+            autoChrome.eval("document.getElementById('add-to-cart-button-in-stock').click()");
+            TimeUnit.SECONDS.sleep(10);
+        }
+        JOptionPane.showMessageDialog(null, "...");
     }
 }
