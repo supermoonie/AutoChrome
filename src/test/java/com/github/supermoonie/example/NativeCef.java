875753aeef308f33126ba0632ca193a076d56052
wang_chao <chao.wang@guanaitong.com>
2021-08-30 18:19:42 +0800

    update

--- src/test/java/com/github/supermoonie/example/NativeCef.java
+++ src/test/java/com/github/supermoonie/example/NativeCef.java
@@ -22,15 +22,8 @@ public class NativeCef {
 
     @Test
     public void nc() throws Exception {
-        AutoChrome autoChrome = new AutoChrome(9222, "about:blank", 5_000);
-        for (int i = 0; i < 1; i++) {
-            autoChrome.navigate("https://www.hermes.com/hk/en/product/mosaique-au-24-mini-bag-H079054CKP9/");
-            autoChrome.waitCondition(autoChrome1 -> (boolean) autoChrome1.eval("!!document.getElementById('add-to-cart-button-in-stock')"), 5_000);
-            TimeUnit.SECONDS.sleep(6);
-//            autoChrome.eval("document.getElementById('add-to-cart-button-in-stock').click()");
-//            TimeUnit.SECONDS.sleep(10);
-        }
-        JOptionPane.showMessageDialog(null, "...");
+        AutoChrome autoChrome = new AutoChrome.Builder().setPort(9222).build();
+        autoChrome.navigate("https://www.hermes.com/fr/fr");
     }
 
     @Test
